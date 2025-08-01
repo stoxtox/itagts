7d53786df4f5235786a80d24639195fc33a9cc51
stoxtox <105239440+stoxtox@users.noreply.github.com>
2025-08-01 13:26:50 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAvymocB8adMv1Z4TJzfmD8aS0bB4bwrw8",
-    authDomain: "itagts.firebaseapp.com",
-    projectId: "itagts",
-    storageBucket: "itagts.firebasestorage.app",
-    messagingSenderId: "763913638158",
-    appId: "1:763913638158:web:a959918d1940ca2aec475f",
-    measurementId: "G-93Z6VMFT49"
+// ✅ add Firebase and Firestore info
   };
 
 // ✅ Initialize Firebase and Firestore
